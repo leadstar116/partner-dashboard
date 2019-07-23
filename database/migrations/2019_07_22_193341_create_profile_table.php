4a9f211a3f0f490f6eecccdc7c784eeea7d997cf
leadstar116 <supremedev116@gmail.com>
2019-07-23 16:57:57 +1000

    db migration made for profile, departments and employees table

---
+++ database/migrations/2019_07_22_193341_create_profile_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfileTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profile', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->string('photo', 255);
+            $table->string('name', 255);
+            $table->string('tagline', 255);
+            $table->string('email', 255);
+            $table->string('phone', 255);
+            $table->enum('industry', ['']);
+            $table->string('address', 255);
+            $table->string('city', 255);
+            $table->string('country', 255);
+            $table->string('state', 255);
+            $table->string('zipcode', 255);
+            $table->string('biz_description', 1024);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profile');
+    }
+}
---
+++ database/migrations/2019_07_23_064137_change_user_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (Schema::hasTable('users') && !Schema::hasColumn('users', 'profile_id')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->integer('profile_id');
+                $table->boolean('profile_completed');
+            });
+        }
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('profile_id');
+            $table->dropColumn('profile_completed');
+        });
+    }
+}
